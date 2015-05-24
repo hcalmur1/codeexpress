da0bacb67492f0e9d828a3d3b70a4462e3c28882
hcalmur1 <hcalmur@gmail.com>
2015-05-24 15:13:24 -0500

    Mis primeras adecuaciones y modificaciones de mi proyecto

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Este es mi primer programa con \n nodejs \ny\n Express' });
 });
 
 module.exports = router;
