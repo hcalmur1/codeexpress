2e035a7d5e0f967acab8cfec5fa4f0f69ba20516
hcalmur1 <hcalmur@gmail.com>
2015-05-24 15:53:09 -0500

    Primeras modificaciones
    Aprendiendo a modificar el archivo de las vistas, primeros pasos usando
    nodejs y el framework Express

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Este es mi primer programa con \n nodejs \ny\n Express' });
+  res.render('index', { title: 'Este es mi primer programa con nodejs y Express' });
 });
 
 module.exports = router;
